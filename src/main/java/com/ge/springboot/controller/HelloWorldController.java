ce11043cf0d4365227d5657925409b3fa423e479
Jemyh <492930364@qq.com>
2018-05-18 13:02:39 +0800

    merge changes

--- src/main/java/com/ge/springboot/controller/HelloWorldController.java
+++ src/main/java/com/ge/springboot/controller/HelloWorldController.java
@@ -1,16 +1,15 @@
 package com.ge.springboot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWorldController {
 
-    @RequestMapping("/say")
+    @RequestMapping("/hello")
     public String Say(){
-        System.out.println("执行controller方法");
-        System.out.println("执行controller方法");
-        return "hello";
+        System.out.println("执行controller方法111");
+        return "index";
 
     }
 
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 502774066
+  Date: 5/18/2018
+  Time: 11:45 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+<h2>Welcome</h2>
+</body>
+</html>
