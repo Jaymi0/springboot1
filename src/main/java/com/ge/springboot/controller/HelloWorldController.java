5f4bc3b5e7382bb4bcc26efa93bbd5c05a5e4c1c
Jemyh <492930364@qq.com>
2018-05-18 11:28:24 +0800

    add controller to test springboot helloworld

---
+++ src/main/java/com/ge/springboot/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.ge.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/say")
+    public String Say(){
+        System.out.println("执行controller方法");
+        return "hello";
+
+    }
+
+}
